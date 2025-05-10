8a103703769d931153ad85dc7350ecce872bdff1
wenyu2006211 <wenyu2006211@icloud.com>
2025-05-11 04:02:40 +0800

    SECOND PUSH. FIRST TIME OPTIMIZATION

--- hm3.c
+++ hm3.c
@@ -7,10 +7,11 @@
 #define COLUMNS 9
 
 void displaySeats(char seats[ROWS][COLUMNS]) {
+    int i, j;
     printf("\n   \123456789\n");
-    for (int i = 0; i < ROWS; i++) {
+    for (i = 0; i < ROWS; i++) {
         printf("%2d ", ROWS - i);
-        for (int j = 0; j < COLUMNS; j++) {
+        for (j = 0; j < COLUMNS; j++) {
             printf("%c", seats[i][j]);
         }
         printf("\n");
@@ -21,39 +22,39 @@ bool isValidSeat(int row, int col, char seats[ROWS][COLUMNS]) {
     return row >= 0 && row < ROWS && col >= 0 && col < COLUMNS && seats[row][col] == '-';
 }
 
-void bookSeatAutomatically(char seats[ROWS][COLUMNS], int count) {
+void bookSeats(char seats[ROWS][COLUMNS], int count, bool autoAssign) {
     int booked = 0;
-    for (int i = 0; i < ROWS && booked < count; i++) {
-        for (int j = 0; j < COLUMNS && booked < count; j++) {
-            if (seats[i][j] == '-') {
-                seats[i][j] = '*';
+    int i, j;
+    if (autoAssign) {
+        for (i = 0; i < ROWS && booked < count; i++) {
+            for (j = 0; j < COLUMNS && booked < count; j++) {
+                if (seats[i][j] == '-') {
+                    seats[i][j] = '*';
+                    booked++;
+                }
+            }
+        }
+    } else {
+        while (booked < count) {
+            int row, col;
+            printf("\nEnter the row and column (e.g., 1 2): ");
+            scanf("%d %d", &row, &col);
+            row = ROWS - row;
+            col--;
+            if (isValidSeat(row, col, seats)) {
+                seats[row][col] = '@';
                 booked++;
+            } else {
+                printf("\nInvalid seat selection. Try again.\n");
             }
         }
     }
+
     if (booked == count) {
         printf("\nSeats successfully booked.\n");
     } else {
         printf("\nNot enough seats available.\n");
     }
-}
-
-void bookSeatManually(char seats[ROWS][COLUMNS], int count) {
-    int booked = 0;
-    while (booked < count) {
-        int row, col;
-        printf("\nEnter the row and column (e.g., 1 2): ");
-        scanf("%d %d", &row, &col);
-        row = ROWS - row;
-        col--;
-        if (isValidSeat(row, col, seats)) {
-            seats[row][col] = '@';
-            booked++;
-        } else {
-            printf("\nInvalid seat selection. Try again.\n");
-        }
-    }
-    printf("\nSeats successfully booked.\n");
 }
 
 void menu() {
@@ -69,43 +70,52 @@ int main() {
     char seats[ROWS][COLUMNS];
     memset(seats, '-', sizeof(seats));
     char choice;
-    while (true) {
+    while (1) {
         menu();
         printf("\nEnter your choice: ");
         scanf(" %c", &choice);
 
-        if (choice == 'a') {
-            displaySeats(seats);
-        } else if (choice == 'b') {
-            int num;
-            printf("\nEnter the number of seats to book (1-4): ");
-            scanf("%d", &num);
-            if (num >= 1 && num <= 4) {
-                bookSeatAutomatically(seats, num);
+        switch (choice) {
+            case 'a':
                 displaySeats(seats);
-            } else {
-                printf("\nInvalid number of seats.\n");
-            }
-        } else if (choice == 'c') {
-            int num;
-            printf("\nEnter the number of seats to book (1-4): ");
-            scanf("%d", &num);
-            if (num >= 1 && num <= 4) {
-                bookSeatManually(seats, num);
-                displaySeats(seats);
-            } else {
-                printf("\nInvalid number of seats.\n");
-            }
-        } else if (choice == 'd') {
-            char confirm;
-            printf("\nContinue? (y/n): ");
-            scanf(" %c", &confirm);
-            if (confirm == 'n') {
-                printf("\nExiting the system. Goodbye!\n");
+                break;
+            case 'b': {
+                int num;
+                printf("\nEnter the number of seats to book (1-4): ");
+                scanf("%d", &num);
+                if (num >= 1 && num <= 4) {
+                    bookSeats(seats, num, 1);
+                    displaySeats(seats);
+                } else {
+                    printf("\nInvalid number of seats.\n");
+                }
                 break;
             }
-        } else {
-            printf("\nInvalid choice. Try again.\n");
+            case 'c': {
+                int num;
+                printf("\nEnter the number of seats to book (1-4): ");
+                scanf("%d", &num);
+                if (num >= 1 && num <= 4) {
+                    bookSeats(seats, num, 0);
+                    displaySeats(seats);
+                } else {
+                    printf("\nInvalid number of seats.\n");
+                }
+                break;
+            }
+            case 'd': {
+                char confirm;
+                printf("\nContinue? (y/n): ");
+                scanf(" %c", &confirm);
+                if (confirm == 'n') {
+                    printf("\nExiting the system. Goodbye!\n");
+                    return 0;
+                }
+                break;
+            }
+            default:
+                printf("\nInvalid choice. Try again.\n");
+                break;
         }
     }
 
